263911dd431856dbd0b44e1f669447a08d2a4bf5
ppyang010 <644724460@qq.com>
2017-07-12 17:22:04 +0800

    时间工具类

---
+++ mywaptest/src/org/mywap/util/date/package-info.java
@@ -0,0 +1,15 @@
+/**     
+ * @FileName: package-info.java   
+ * @Package:org.mywap.util.date   
+ * @Description: 日期相关工具类 
+ * @author: s    
+ * @date:2017年7月12日 下午2:20:24   
+ * @version V1.0     
+ */
+/**  
+ * @ClassName: package-info   
+ * @Description: TODO  
+ * @author: s  
+ * @date:2017年7月12日 下午2:20:24     
+ */
+package org.mywap.util.date;
